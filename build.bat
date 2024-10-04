e35cf8dfe4451c1d7cdf0fa83d56e6dfa6f225a1
karthikeyan_thangave <pt.karthik1989@gmail.com>
2024-10-04 12:24:28 +0530

    build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,16 @@
 @echo off
 echo Starting the build process...
 
+:: Set classpath to include TestNG and other dependencies
+set CLASSPATH=bin;path\to\testng.jar;path\to\other\dependencies\*.jar
+
+:: Creating the output directory if it doesn't exist
+if not exist bin (
+    mkdir bin
+)
+
 :: Compiling the Java program
-javac -d bin src\test\java\IndustryStandard\MyFirstTest.java
+javac -d bin -cp %CLASSPATH% main\IndustryStandard\MyFirstTest.java main\driver\DriverCreator.java main\listeners\BasicListeners.java
 if %errorlevel% neq 0 (
     echo Build failed!
     exit /b 1
@@ -13,6 +21,6 @@ if %errorlevel% neq 0 (
 :: Running the compiled Java program
 echo Running the program...
 cd bin
-java -cp "bin;path\to\testng.jar" IndustryStandard.MyFirstTest
+java -cp "%CLASSPATH%;" IndustryStandard.MyFirstTest
 
 echo Build process completed!
